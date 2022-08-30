615eb6c8c54ccd28632fb93a6205e1eaf0b300de
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-08-30 09:54:40 -0400

    implement GraphQL

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+// import apollo server
+const { ApolloServer } = require("apollo-server-express");
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// create a new instance of an Apollo server with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our apollo server with the Exress application as middleware
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
-});
+};
+
+// call the async function to start the server
+startApolloServer(typeDefs, resolvers);
