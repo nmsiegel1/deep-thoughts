e0bd7e1cbf4cf447b687a262928fcdaab91055f1
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-08-30 15:41:34 -0400

    set up react-app and integrate the client with the server. set up concurent servers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // import apollo server
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -25,6 +26,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our apollo server with the Exress application as middleware
   server.applyMiddleware({ app });
+
+  // serve up static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
